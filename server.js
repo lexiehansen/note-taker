55ce1271eb4198a856b96a142e5a08149859febd
Lexie Hansen <home@Lexies-MacBook-Pro.local>
2021-02-15 19:09:05 -0700

    add routes folder and routesJS, add dependencies to routes, add middleware and route to serverJS

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
 
+require('./routes/routes')(app);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`)
